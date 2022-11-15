80a756469039b5bcc284c2b51137cdfd998a2752
Carl-with-a-C <carlbclifton@gmail.com>
2022-11-15 11:48:20 +0000

    4 Get articles complete. tested body object for key/values and data types. tested for 404 path error

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/getTopics");
+const { getArticles } = require("./controllers/getArticles");
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles", getArticles);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "path not found!" });
