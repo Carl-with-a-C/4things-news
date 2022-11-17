4d0de0b9f774293a3dde42d989c2fc73df416510
Carl-with-a-C <carlbclifton@gmail.com>
2022-11-17 09:24:42 +0000

    as per emily removed confusing testData require in

--- __tests__/article_comments.test.js
+++ __tests__/article_comments.test.js
@@ -1,13 +1,12 @@
 const seed = require("../db/seeds/seed");
 const db = require("../db/connection");
-const articleData = require("../db/data/test-data/index");
-const userData = require("../db/data/test-data/index");
+const testData = require("../db/data/test-data/index");
 const request = require("supertest");
 const app = require("../app");
 const sorted = require("jest-sorted");
 
 beforeEach(() => {
-  return seed(articleData, userData);
+  return seed(testData);
 });
 
 afterAll(() => {
