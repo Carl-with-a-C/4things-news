dc63e871053e3651bc8f3f2be5c466ab91b1ad98
Carl-with-a-C <carlbclifton@gmail.com>
2022-12-06 11:57:46 +0000

    added body to GET articles 3rd attempt

--- models/fetchArticles.js
+++ models/fetchArticles.js
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
 exports.fetchArticles = () => {
-  const queryStr = `SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, body COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`;
+  const queryStr = `SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, body, COUNT(comments.article_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`;
   // SELECT COUNT(article_id) AS comment_count FROM comments;`;
   return db.query(queryStr).then((foundArticles) => {
     return foundArticles.rows;
